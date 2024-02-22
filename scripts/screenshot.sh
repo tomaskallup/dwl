8674d9ee6771cb64c1271894a6f7133048a9e192
Tomas Kallup <t.kallup@gmail.com>
2024-02-22 12:02:14 +0100

    Include cursor in screeshots

--- scripts/screenshot.sh
+++ scripts/screenshot.sh
@@ -5,11 +5,11 @@ rm -f $screenshot_output
 if [ "$1" = "select" ]; then
   # Let me select screen to screenshot
   slurp_output=$(slurp -c '#ff0000ff' -ro)
-  grim -g "$slurp_output" - > $screenshot_output
+  grim -cg "$slurp_output" - > $screenshot_output
 else
   # Take current active screen
   active_screen=$(tac ~/.dwl_info | grep -m1 "selmon 1" | sed -e 's/ .*//')
-  grim -o $active_screen - > $screenshot_output
+  grim -co $active_screen - > $screenshot_output
 fi
 
 if [ -s "$screenshot_output" ]; then
